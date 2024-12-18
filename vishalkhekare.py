3b1814ea9c69329deae5affc7ff24a2589c6b72c
vishalkhekare07 <vishalkhekare07@gmail.com>
2024-12-18 05:58:14 -0800

    Create vishalkhekare.py
    1. Remove duplicates and sort the list; the second last element is the second largest.
    2. Alternatively, traverse the list, tracking the largest and second largest numbers simultaneously.

---
+++ vishalkhekare.py
@@ -0,0 +1,7 @@
+def second_largest(numbers):
+  unique_numbers = list(set(numbers))                
+  unique_numbers.sort()                                      
+  return unique_numbers[-2]                               
+numbers = [10, 20, 4, 45, 99]
+output = second_largest(numbers)
+print(output)
